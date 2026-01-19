04b048bbb7d2dce7535746df1bdb98a7dd5a5891
Dorcas Okeleji <okelejidorcas@gmail.com>
2026-01-19 16:52:31 +0100

    correct the color error

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
             <footer style= {{ fontSize: '15px'}}>
                 <p>© 2023 City Lovers</p>
             </footer>
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
     return (
-        <div syle= {{ border: '1px solid navy', padding: '10px', margin: '10px',}}>
-            <h2 style= {{ color: 'green' }}>{props.name}</h2>
+        <div syle= {{ border: '1px solid gray', padding: '10px', margin: '10px',}}>
+            <h2 style= {{ color: 'blue' }}>{props.name}</h2>
             <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
