4e9268e3abee332c0db686c8447360b38f8acf1d
Dorcas Okeleji <okelejidorcas@gmail.com>
2026-01-19 16:47:16 +0100

    add inline styling to the components files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <main style= {{ fontSize: '20px', boxContainer: '0, 20px, rgb 0, 0, 0, 0.2', }}>
+                <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;
