92c0d967a1131a0c8a6abd7ab64b335cfad3ed8d
Dorcas Okeleji <okelejidorcas@gmail.com>
2026-02-12 11:21:57 +0100

    add BrowserRouter to App.jsx file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage.jsx';
 import RecipeDetail from './components/RecipeDetail.jsx';
 import AddRecipeForm from './components/AddRecipeForm.jsx';
 
 function App() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path='/add' element={<AddRecipeForm />} />
-      </Routes>
-    
+    <BrowserRouter>
+      <div className='min-h-screen bg-gray-50'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path='/add' element={<AddRecipeForm />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
