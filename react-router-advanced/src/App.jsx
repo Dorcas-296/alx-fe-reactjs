4536a6669a7877e27c6602147b437dae0489acab
Dorcas Okeleji <okelejidorcas@gmail.com>
2026-03-03 14:49:39 +0100

    fix name error

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Home from "./component/Home.jsx";
-import Profile from "./component/Profile.jsx";
-import ProfileDetails from "./component/ProfileDetails.jsx";
-import ProfileSettings from "./component/ProfileSettings.jsx";
-import BlogPost from "./component/BlogPost.jsx";
-import Login from "./component/Login.jsx";
+import Home from "./components/Home.jsx";
+import Profile from "./components/Profile.jsx";
+import ProfileDetails from "./components/ProfileDetails.jsx";
+import ProfileSettings from "./components/ProfileSettings.jsx";
+import BlogPost from "./components/BlogPost.jsx";
+import Login from "./components/Login.jsx";
 
 
   const profileAuth = { isAuthenticated: false };
